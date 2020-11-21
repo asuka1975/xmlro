df1148f41309988a9265fc86e176da1ebec42f6d
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2020-11-21 21:54:58 +0900

    added xmlro.h for the root header file

--- src/dummy.cpp
+++ src/dummy.cpp
@@ -2,3 +2,4 @@
 // Created by hungr on 2020/11/21.
 //
 
+#include "xmlro/xmlro.h"
---
+++ xmlro/xmlro.h
@@ -0,0 +1,8 @@
+//
+// Created by hungr on 2020/11/21.
+//
+
+#ifndef XMLRO_XMLRO_H
+#define XMLRO_XMLRO_H
+
+#endif //XMLRO_XMLRO_H
