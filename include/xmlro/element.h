db06af30ce226e98afdff499f234d2db85b9f925
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2020-11-22 23:06:30 +0900

    added element.h

---
+++ include/xmlro/element.h
@@ -0,0 +1,17 @@
+//
+// Created by hungr on 2020/11/22.
+//
+
+#ifndef XMLRO_ELEMENT_H
+#define XMLRO_ELEMENT_H
+
+namespace xml {
+    class element {
+    public:
+        constexpr element() = default;
+    private:
+
+    };
+}
+
+#endif //XMLRO_ELEMENT_H
--- include/xmlro/xmlro.h
+++ include/xmlro/xmlro.h
@@ -5,4 +5,6 @@
 #ifndef XMLRO_XMLRO_H
 #define XMLRO_XMLRO_H
 
+#include "element.h"
+
 #endif //XMLRO_XMLRO_H
